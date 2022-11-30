19fbd71a832c94401e9180806596a347fb571dcc
Rajanikanth suthrapu <rsuthrapu@ciginsurance.com>
2022-11-30 09:27:11 -0800

    QUERY

--- Dynamic_column_generate.sql
+++ Dynamic_column_generate.sql
@@ -2,11 +2,13 @@ create or replace FUNCTION GetTableScript(TAB_NAME IN VARCHAR2)
 RETURN CLOB IS 
  columnList  CLOB;
 BEGIN
-        select 
-        rtrim(xmlagg(xmlelement(e,column_name || '  ' || data_type || 
-                   '('|| data_length||')',', ').extract('//text()') order by column_name).getclobval(),', ') str
-        into columnList
-        from all_tab_columns@ECIG_TO_CC_LINK C
-        where rownum <= 500 AND C.TABLE_NAME = TAB_NAME;
+  select 
+    regexp_replace(rtrim(xmlagg(xmlelement(e,column_name || '  ' || data_type || 
+               case when data_type not like('%TIMESTAMP%') then 
+                     '('|| data_length ||')' end ,', '|| chr(10))
+                     .extract('//text()') order by column_name).getclobval(),', '), ',([^,]*)$', '\1') str
+    into columnList
+    from all_tab_columns@ECIG_TO_CC_LINK C
+    where rownum <= 500 AND C.TABLE_NAME = TAB_NAME AND OWNER = 'CCADMIN02';
     return columnList;
 END;
