5790a27da9a5dbe79b2f8c398ee67ba87ff11ca2
Rajanikanth Suthrapu <rsuthrapu@ciginsurance.com>
2025-02-26 13:02:03 -0800

    latest

--- DWH/CLASS_CODE_DATA.sql
+++ DWH/CLASS_CODE_DATA.sql
@@ -3,6 +3,7 @@ CREATE TABLE CLASS_CODE_DATA as
                     SELECT
                     BP7CLASSCODE,
                     C.BRANCHID,
+                    PP.POLICYNUMBER,
                     ROW_NUMBER() OVER(PARTITION BY C.BRANCHID, C.BUILDING
                                                     ORDER BY C.BUILDING, C.BP7CLASSCODE) AS BUILDING_ROWNUM
                     FROM  PCADMIN.PCX_BP7CLASSIFICATION C
@@ -10,8 +11,7 @@ CREATE TABLE CLASS_CODE_DATA as
                     ON PP.ID=C.BRANCHID 
                     ),
                     CLASS_CODE_RECORD AS(
-                        SELECT * FROM CLASS_CODE_DATA
-                        WHERE  BUILDING_ROWNUM < 2
+                        SELECT * FROM CLASS_CODE_DATA WHERE  BUILDING_ROWNUM < 2
                     )
                     select * from CLASS_CODE_RECORD;
 
